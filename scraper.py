61313015086a68cda4c85086edd4fe8e7deeab48
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2016-08-09 10:34:48 +1200

    Replacing try/catch by utility methods

--- scraper.py
+++ scraper.py
@@ -27,6 +27,8 @@ def post(session, url, data):
     return response
 
 def main():
+    """Main method"""
+
     # dotEnv
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
@@ -50,53 +52,41 @@ def main():
     # Start a requests session. The session takes care of passing the cookies
     # to the next requests.
     with requests.Session() as s:
-        # log in
+        # Log in
         login_url = 'https://login.solar-inverter.com/en-US/Account/SignIn?returnUrl=%252fissue%252fwsfed%253fwa%253dwsignin1.0%2526wtrealm%253dhttp%25253a%25252f%25252fsoliviamonitoring.com%25252f%2526wctx%253drm%25253d0%252526id%25253dpassive%252526ru%25253d%2525252f%2526wct%253d' + now_ts_enc
-        r = s.post(login_url, data={'Email': SOLIVIA_USER, 'Password': SOLIVIA_PASS, 'RememberMe': 'false'})
-        if r.status_code != requests.codes.ok:
-            r.raise_for_status()
+        data = {'Email': SOLIVIA_USER, 'Password': SOLIVIA_PASS, 'RememberMe': 'false'}
+        r = post(s, login_url, data)
 
-        redirect_url = 'https://login.solar-inverter.com/issue/wsfed?wa=wsignin1.0&wtrealm=http%3a%2f%2fsoliviamonitoring.com%2f&wctx=rm%3d0%26id%3dpassive%26ru%3d%252f&wct=' + now_ts_enc
         # Redirect post login
-        r = s.get(redirect_url)
-        if r.status_code != requests.codes.ok:
-            r.raise_for_status()
+        redirect_url = 'https://login.solar-inverter.com/issue/wsfed?wa=wsignin1.0&wtrealm=http%3a%2f%2fsoliviamonitoring.com%2f&wctx=rm%3d0%26id%3dpassive%26ru%3d%252f&wct=' + now_ts_enc
+        r = get(s, redirect_url)
         text_with_ws_result = r.text
 
         # Azure AD auth
         wresult = re.search('%s(.*)%s' % ('<input type="hidden" name="wresult" value="', '" /><input type="hidden" name="wctx"'), text_with_ws_result).group(1)
         wresult = html.unescape(wresult)
-        r = s.post('https://monitoring.solar-inverter.com/', data={'wa': 'wsignin1.0', 'wresult': wresult, 'wctx': 'rm=0&id=passive&ru=%2f'})
-        if r.status_code != requests.codes.ok:
-            r.raise_for_status()
+        data = {'wa': 'wsignin1.0', 'wresult': wresult, 'wctx': 'rm=0&id=passive&ru=%2f'}
+        r = post(s, 'https://monitoring.solar-inverter.com/', data)
 
+        # Set the context date
         set_date_url = 'https://monitoring.solar-inverter.com/Chart/SetXConfig?date=06%2F08%2F2016'
-        fetch_inverter_data_url = 'https://monitoring.solar-inverter.com/Chart/FetchInverterData?duration=Daily'
-        set_parameters_url = 'https://monitoring.solar-inverter.com/Chart/SetYConfig?invList=' + inverter1 + '%3B' + inverter2 + '%3B&dataType=Power&yMult=1'
-
-        # Set date
-        r = s.get(set_date_url)
-        if r.status_code != requests.codes.ok:
-            r.raise_for_status()
+        r = get(s, set_date_url)
 
         # Fetch inverter data
-        r = s.post(fetch_inverter_data_url, data={'sort': '', 'group': '', 'filter': '', 'duration': 'Daily'})
-        if r.status_code != requests.codes.ok:
-            r.raise_for_status()
+        fetch_inverter_data_url = 'https://monitoring.solar-inverter.com/Chart/FetchInverterData?duration=Daily'
+        data = {'sort': '', 'group': '', 'filter': '', 'duration': 'Daily'}
+        r = post(s, fetch_inverter_data_url, data)
 
         # Set other parameters
-        r = s.get(set_parameters_url)
-        if r.status_code != requests.codes.ok:
-            r.raise_for_status()
+        set_parameters_url = 'https://monitoring.solar-inverter.com/Chart/SetYConfig?invList=' + inverter1 + '%3B' + inverter2 + '%3B&dataType=Power&yMult=1'
+        r = get(s, set_parameters_url)
 
-        # Get data
-        # get data URL + the Solivia plant GUID
+        # Get data URL + the Solivia plant GUID
         get_data_url = 'https://monitoring.solar-inverter.com/Chart/FetchChartData?duration=Daily&datatype=Power&plantGuid=' + SOLIVIA_PLANTGUID
-        r = s.get(get_data_url)
-        if r.status_code != requests.codes.ok:
-            r.raise_for_status
+        r = get(s, get_data_url)
 
         print(r.text)
 
 if __name__ == '__main__':
+    """App entry point"""
     main()
